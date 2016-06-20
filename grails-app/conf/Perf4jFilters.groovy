315ac575f3e5aa6622d6d0a56980293c48cc6986
Tung NGUYEN <nvntung@gmail.com>
2016-06-20 21:16:54 +0100

    clean up trailing spaces and lines

--- grails-app/conf/Perf4jFilters.groovy
+++ grails-app/conf/Perf4jFilters.groovy
@@ -3,7 +3,6 @@ import grails.util.GrailsNameUtils as GNU
 import org.perf4j.log4j.Log4JStopWatch
 import org.apache.log4j.Logger
 
-
 public class Perf4jFilters {
     // the name of the config property in controllers
     static final String PROFILED_PROPERTY = "profiled"
@@ -16,80 +15,71 @@ public class Perf4jFilters {
 
     def controllerProfiledOptionsCache
     
-    
     def filters = {
         def log = Logger.getLogger(Perf4jFilters)
         all(controller:'*', action: '*') {
-        try {
-            def pluginDescriptor = Class.forName("Perf4jGrailsPlugin")?.newInstance()
-            before = {
-                if(pluginDescriptor.profilingEnabled && pluginDescriptor.profilingCurrentlyEnabled) {
-                    if(controllerName) {
-                        def action = actionName ?: 'index'
-                        def controller = GNU.getClassName(controllerName, "Controller")
+            try {
+                def pluginDescriptor = Class.forName("Perf4jGrailsPlugin")?.newInstance()
+                before = {
+                    if (pluginDescriptor.profilingEnabled && pluginDescriptor.profilingCurrentlyEnabled) {
+                        if (controllerName) {
+                            def action = actionName ?: 'index'
+                            def controller = GNU.getClassName(controllerName, "Controller")
+                            def controllerClass = grailsApplication.getArtefactByLogicalPropertyName("Controller", controllerName)
+                            if(controllerClass) {
+                                def profiled = GCU.getStaticPropertyValue(controllerClass.clazz, PROFILED_PROPERTY)
 
-                        def controllerClass = grailsApplication.getArtefactByLogicalPropertyName("Controller", controllerName)
-                        if(controllerClass) {
-                            def profiled = GCU.getStaticPropertyValue(controllerClass.clazz, PROFILED_PROPERTY)
-
-                            if(profiled instanceof Boolean && profiled) {
-                                log.trace "Boolean type profiled property in ${controller}"
-                                createStopwatch(null, null, false, controller, action, request)
-                            }
-                            else if(profiled instanceof List && profiled.contains(action)) {
-                                log.trace "Collection type profiled property in ${controller}"
-                                createStopwatch(null, null, false, controller, action, request)
-                            }
-                            else if(profiled instanceof Closure) {
-                                log.trace "Closure type profiled property in ${controller}"
-
-                                if(!controllerProfiledOptionsCache.hasOptionsForController(controllerName)) {
-                                    log.trace "Evaluating profiled DSL in ${controller}"
-                                    controllerProfiledOptionsCache.evaluateDSL(controllerName, profiled)
+                                if (profiled instanceof Boolean && profiled) {
+                                    log.trace "Boolean type profiled property in ${controller}"
+                                    createStopwatch(null, null, false, controller, action, request)
                                 }
-                                else {
-                                    log.trace "Using cached profiling options for ${controller}"
+                                else if (profiled instanceof List && profiled.contains(action)) {
+                                    log.trace "Collection type profiled property in ${controller}"
+                                    createStopwatch(null, null, false, controller, action, request)
                                 }
+                                else if (profiled instanceof Closure) {
+                                    log.trace "Closure type profiled property in ${controller}"
 
-                                def options = controllerProfiledOptionsCache.getOptions(controllerName, action)
-                                if(options) {
-                                    createStopwatch(options.tag, options.message, options.includeView as Boolean, controller, action, request)
+                                    if (!controllerProfiledOptionsCache.hasOptionsForController(controllerName)) {
+                                        log.trace "Evaluating profiled DSL in ${controller}"
+                                        controllerProfiledOptionsCache.evaluateDSL(controllerName, profiled)
+                                    }
+                                    else {
+                                        log.trace "Using cached profiling options for ${controller}"
+                                    }
+
+                                    def options = controllerProfiledOptionsCache.getOptions(controllerName, action)
+                                    if (options) {
+                                        createStopwatch(options.tag, options.message, options.includeView as Boolean, controller, action, request)
+                                    }
                                 }
                             }
                         }
                     }
                 }
-            }
-
-            
-            after = {
-                if(pluginDescriptor.profilingEnabled && pluginDescriptor.profilingCurrentlyEnabled) {
-                    def includeView = request[INCLUDE_VIEW_REQUEST_KEY]
-            
-                    if(!includeView) {
-                        stopStopwatch(request)
+                after = {
+                    if (pluginDescriptor.profilingEnabled && pluginDescriptor.profilingCurrentlyEnabled) {
+                        def includeView = request[INCLUDE_VIEW_REQUEST_KEY]
+                        if (!includeView) {
+                            stopStopwatch(request)
+                        }
                     }
                 }
-            }
-
-
-            afterView = {
-                if(pluginDescriptor.profilingEnabled && pluginDescriptor.profilingCurrentlyEnabled) {
-                    def includeView = request[INCLUDE_VIEW_REQUEST_KEY]
-
-                    if(includeView) {
-                        stopStopwatch(request)
+                afterView = {
+                    if (pluginDescriptor.profilingEnabled && pluginDescriptor.profilingCurrentlyEnabled) {
+                        def includeView = request[INCLUDE_VIEW_REQUEST_KEY]
+                        if (includeView) {
+                            stopStopwatch(request)
+                        }
                     }
                 }
+            } catch (Throwable e) {
+                log.error("Cannot find class Perf4jGrailsPlugin", e)
+                return
             }
-        } catch (Throwable e) {
-            log.error("Cannot find class Perf4jGrailsPlugin - Double check", e)
-            return
-        }
         }
     }
-    
-    
+
     /**
      *  Create the stop watch and store it in the request, so it can be accessed in the after/afterView interceptors.
      */
